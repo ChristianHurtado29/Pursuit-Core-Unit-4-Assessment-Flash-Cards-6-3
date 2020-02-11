1f4486f4b9a1303a6fdbf1d2b7bdb137d72c77a8
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-11 10:18:22 -0500

    retrieved swift packages, switched out main view controller for tab bar controller programmatically

---
+++ Unit4Assessment/Unit4Assessment/InitialVC.swift
@@ -0,0 +1,21 @@
+//
+//  InitialVC.swift
+//  Unit4Assessment
+//
+//  Created by Christian Hurtado on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
