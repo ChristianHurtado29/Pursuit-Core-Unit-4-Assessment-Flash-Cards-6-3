52b786e25c30cb6dbf76d688ec400d2d1fb0d392
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-12 10:58:45 -0500

    finished cards cell logic

--- Unit4Assessment/Unit4Assessment/Cells/CardsCell.swift
+++ Unit4Assessment/Unit4Assessment/Cells/CardsCell.swift
@@ -18,7 +18,7 @@ class CardsCell: UICollectionViewCell {
     
     public var selCard: Cards!
     
-    public var isPressed = false
+    public var isAnswer = false
     
     private lazy var longPressGesture: UILongPressGestureRecognizer = {
         let gesture = UILongPressGestureRecognizer()
@@ -30,15 +30,29 @@ class CardsCell: UICollectionViewCell {
         let label = UILabel()
         label.numberOfLines = 2
         label.font = UIFont.preferredFont(forTextStyle: .headline)
-        label.text = "Question title"
+        label.text = "Question title" // remove after
         return label
     }()
     
-    public lazy var ellipsisButton: UIButton = {
+    public lazy var answersLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.text = "Answers" // remove after
+        return label
+    }()
+    
+    public lazy var optionsButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "ellipsis"), for: .normal)
-        button.addTarget(self, action: #selector(ellipButtonPressed(_:)), for: .touchUpInside)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.addTarget(self, action: #selector(optionsButtonPressed(_:)), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var longPressedGesture: UILongPressGestureRecognizer = {
+      let gesture = UILongPressGestureRecognizer()
+      gesture.addTarget(self, action: #selector(didLongPress(_:)))
+      return gesture
     }()
     
     private lazy var tapGesture: UITapGestureRecognizer = {
@@ -51,24 +65,91 @@ class CardsCell: UICollectionViewCell {
         if gesture.state == .began || gesture.state == .changed {
             return
         }
-        isPressed.toggle()
+        isAnswer.toggle()
         animate()
     }
     
-    @objc public func ellipButtonPressed(_ sender: UIButton) {
+    @objc public func optionsButtonPressed(_ sender: UIButton) {
         delegate?.didPressButton(cell: self, card: selCard)
     }
     
+    @objc private func didLongPress(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began || gesture.state == .changed {
+          return
+        }
+        isAnswer.toggle()
+        animate()
+      }
+    
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+      super.init(coder: coder)
+      commonInit()
+    }
+    
+    private func commonInit() {
+        setupLabelConstraints()
+        setupAnswersConstraints()
+        setupButton()
+        addGestureRecognizer(longPressedGesture)
+    }
+    
+    private func setupLabelConstraints() {
+        addSubview(questionLabel)
+        questionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionLabel.topAnchor.constraint(equalTo: topAnchor, constant:  10),
+            questionLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -10),
+            questionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setupAnswersConstraints() {
+        addSubview(answersLabel)
+        answersLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            answersLabel.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 5),
+            answersLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            answersLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setupButton(){
+        addSubview(optionsButton)
+        optionsButton.backgroundColor = .white
+        optionsButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            optionsButton.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            optionsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            optionsButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
     @objc private func animate(){
-        let duration: Double = 1.8
-        let curveOption: UIView.AnimationOptions = .curveEaseInOut
-        
-        // the four built-in bezier animations curves
-        // curveEaseIn
-        // curveEaseOut
-        // curveLinear
-        
-        
-      
+        let duration: Double = 1.2
+        if isAnswer{
+            UIView.transition(with: self, duration: duration, options: [.transitionFlipFromLeft], animations: {
+              self.questionLabel.alpha = 0.0
+              self.answersLabel.alpha = 1.0
+            }, completion: nil)
+        } else {
+            UIView.transition(with: self, duration: duration, options: [.transitionFlipFromRight], animations: {
+              self.questionLabel.alpha = 1.0
+              self.answersLabel.alpha = 0.0
+            }, completion: nil)
+        }
+    }
+    
+    
+    public func configureCell(for card: Cards) {
+        questionLabel.text = card.quizTitle
+        answersLabel.text = """
+        1. \(card.facts[0])
+        2. \(card.facts[1])
+        """
     }
 }
